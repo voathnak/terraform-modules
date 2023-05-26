7deec4369c66ade4fe9f9a200e82a42d238a538a
Voathnak Lim <voathnak.lim@rhbgroup.com>
2023-05-26 14:28:09 +0700

    update role name to use format function

--- quick_lambda/role.tf
+++ quick_lambda/role.tf
@@ -28,14 +28,14 @@ data "aws_iam_policy_document" "s3_full_access_assume_role_policy_docs" {
 }
 
 resource "aws_iam_role" "quick_set_lambda_role" {
-  name = "${var.project_name}_${terraform.workspace}_general_lambda_role"
+  name = format("%s_%s_general_%s_lambda_role", var.project_name, terraform.workspace, var.name)
 
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy_docs.json
 }
 
 resource "aws_iam_role_policy" "quick_set_access_lambda_policy" {
   #  attach policy to role
-  name   = "${var.project_name}_${terraform.workspace}_s3_full_access_policy"
+  name   = format("%s_%s_%s_lambda_s3_full_access_policy", var.project_name, terraform.workspace, var.name)
   role   = aws_iam_role.quick_set_lambda_role.id
   policy = data.aws_iam_policy_document.quick_set_access_lambda_policy_docs.json
 }
